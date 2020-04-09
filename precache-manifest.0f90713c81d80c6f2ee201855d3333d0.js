13a5b017e16204a1021b66f132e68d64cdf3be1e
Rimil Dey <rimildeyjsr@gmail.com>
2020-04-09 18:02:22 +0530

    Updates

---
+++ precache-manifest.0f90713c81d80c6f2ee201855d3333d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681092b1e8cc849ffe89c3885224ede7",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f0574782d7800f24ad42",
+    "url": "/todo/static/css/main.238f566d.chunk.css"
+  },
+  {
+    "revision": "d81d3aef6940085cfb7f",
+    "url": "/todo/static/js/2.b4304e36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.b4304e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0574782d7800f24ad42",
+    "url": "/todo/static/js/main.090735bd.chunk.js"
+  },
+  {
+    "revision": "af2c6e9913e1b790c178",
+    "url": "/todo/static/js/runtime-main.6c9cdb5c.js"
+  }
+]);
